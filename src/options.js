c5c851cdb11378d21569771553dd86eeaa668111
wesinator <5124946+wesinator@users.noreply.github.com>
2023-02-09 10:13:19 -0500

    clear custom url when server is selected

--- src/options.js
+++ src/options.js
@@ -1,20 +1,28 @@
 var dns_query_url = "https://cloudflare-dns.com/dns-query";
 
+function clearCustomURL() {
+    document.querySelector("#dns-server-custom").value = "";
+}
+
 function saveOptions(e) {
   e.preventDefault();
   
   // table mapping dns server options to server URLs
   switch(document.querySelector("#dns-server-select").value) {
     case "cloudflare-default":
+      clearCustomURL();
       break;
     case "cleanbrowsing-adult":
+      clearCustomURL();
       dns_query_url = "https://doh.cleanbrowsing.org/doh/adult-filter/";
       break;
     case "google":
+      clearCustomURL();
       // special case with Google: API endpoint, not RFC8484 endpoint
       dns_query_url = "https://dns.google/resolve";
       break;
     case "quad9":
+      clearCustomURL();
       dns_query_url = "https://dns.quad9.net/dns-query";
       break;
     default:
