5ee575731533e5bc93491a9a7ec664b995dfe961
wesinator <5124946+wesinator@users.noreply.github.com>
2023-02-09 00:07:13 -0500

    async, correctly load dns config stor

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-function sendQuery() {
-    var dns_base_url = browser.storage.local.get("dns_url") || "https://cloudflare-dns.com/dns-query";
-    console.log("using DNS server ", dns_base_url);
-    var dns_query_url = dns_base_url + `?name=${document.getElementById("dns-query").text}&type=${document.getElementById("dns-query-type").text}`;
+function loadQuery() {
+    var dns_base_url = "https://cloudflare-dns.com/dns-query";
     
+    dns_base_url = browser.storage.local.get("dns_url").then(
+        // https://stackoverflow.com/questions/29516390/how-can-i-access-the-value-of-a-promise
+        function (result) {
+            console.log("result is:", result);
+            dns_base_url = result.dns_url || dns_base_url;
+            sendQuery(dns_base_url);
+        }).catch(function (error) {
+            sendQuery(dns_base_url);
+        });
+
+function sendQuery(dns_server)
+{
+    console.log("using DNS server ", dns_base_url);
+
+    var dns_query_url = dns_base_url + `?name=${document.getElementById("dns-query").value}&type=${document.getElementById("dns-query-type").value}`;
+
     fetch(dns_query_url, {
       headers: {
         'Content-Type': 'application/dns-json'
@@ -30,9 +44,10 @@ function sendQuery() {
         .catch((error) => {
            console.log(`Error with fetch(): ${error.message}`)
         });
+    }    
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   var clickyButton = document.querySelector('#send-query');
-  clickyButton.addEventListener('click', sendQuery);
+  clickyButton.addEventListener('click', loadQuery);
 });
