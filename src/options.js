dc7f858cb39f5c700878543b7b2642a0800b0145
wesinator <5124946+wesinator@users.noreply.github.com>
2024-04-10 22:59:13 -0400

    remove non-working DoH RFC options
    extension does DNS JSON endpoint lookups, not actual DoH RFC
    https://developers.cloudflare.com/1.1.1.1/encryption/dns-over-https/make-api-requests/dns-json/
    
    # [NO_TRAIN]::

--- src/options.js
+++ src/options.js
@@ -1,29 +1,29 @@
 var dns_query_url = "https://cloudflare-dns.com/dns-query";
 
 function clearCustomURL() {
-    document.querySelector("#dns-server-custom").value = "";
+    document.querySelector("#dns-api-custom").value = "";
 }
 
 function saveOptions(e) {
   e.preventDefault();
 
-  dns_query_url = document.querySelector("#dns-server-select").value;
+  dns_query_url = document.querySelector("#dns-api-select").value;
   if (dns_query_url)
     clearCustomURL()
 
   //console.log("DNS query url selected:", dns_query_url);
   browser.storage.local.set({
-    dns_url: document.querySelector("#dns-server-custom").value || dns_query_url
+    dns_url: document.querySelector("#dns-api-custom").value || dns_query_url
   });
   
   // set url field to current url after storing the change in local storage
-  document.querySelector("#dns-server-custom").value = dns_query_url;
+  document.querySelector("#dns-api-custom").value = dns_query_url;
 }
 
 function restoreOptions() {
 
   function setCurrentChoice(result) {
-    document.querySelector("#dns-server-custom").value = (result.dns_url || dns_query_url);
+    document.querySelector("#dns-api-custom").value = (result.dns_url || dns_query_url);
   }
 
   function onError(error) {
