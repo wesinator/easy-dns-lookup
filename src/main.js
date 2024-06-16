2925f291a9df0cb3b39885ae46c4a925e0ec0ec3
wesinator <5124946+wesinator@users.noreply.github.com>
2024-06-16 00:43:06 -0400

    refactor
    better function structure, use async, https://developer.mozilla.org/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
    remove unneeded ret,
    rename
    
    # NO_TRAIN

--- src/main.js
+++ src/main.js
@@ -21,83 +21,83 @@ readTextFile("./dns-rtypes.json", function(text){
       }
 });
 
-
-function loadQuery() {
-    var dns_base_url = "https://cloudflare-dns.com/dns-query";
-    
-    var hostname = document.getElementById("dns-query").value;
-    var dns_type = document.getElementById("dns-query-type").value;
-    
-    browser.storage.local.get("dns_url").then(
-        // https://stackoverflow.com/questions/29516390/how-can-i-access-the-value-of-a-promise
-        function (result) {
-            dns_base_url = result.dns_url || dns_base_url;
-            return sendQuery(dns_base_url, hostname, dns_type);
-    }).catch(function (error) {
-        return sendQuery(dns_base_url, hostname, dns_type);
-    });
-
-function sendQuery(dns_server_url)
+async function DnsAPIQuery(dns_api_url, query_hostname, query_type)
 {
-    console.log("using DNS server ", dns_server_url);
+    var dns_query_url = dns_api_url + `?name=${query_hostname}&type=${query_type}`;
 
-    var dns_query_url = dns_server_url + `?name=${hostname}&type=${dns_type}`;
-
-    fetch(dns_query_url, {
+    const response = await fetch(dns_query_url, {
       headers: {
         'Accept': 'application/dns-json'
       }
-    }).then((response) => {
-            // Check if the response is ok
-            if (!response.ok) {
-              errMsg = `Error: ${response.status} - ${response.statusText}`;
-              console.log(errMsg);
-              return 1;
-            }
-            // Check if the response is in JSON format
-            if (response.headers.get("Content-Type")
-                .includes("json")) {
-                  response.json().then(
-                      function processResponse(result) {
-                          console.log(result);
-                          var resultStr = "";
-                          if (result.Answer) {
-                              for (answer of result.Answer) {
-                                  var dataFmt = `${answer.name.toString()}, ${answer.data.toString()}`;
-                                  resultStr = resultStr + `${dataFmt}\n`;
-                              }
-                          }
+      
+    });
+    return response;
+}
 
-                          /* This handles cases where response has no answer but a
-                             OPT (RFC6891) message. This includes RFC8914 errors
-                             https://developers.cloudflare.com/1.1.1.1/infrastructure/extended-dns-error-codes/
+async function processQuery() {
+    var dns_base_url = "https://cloudflare-dns.com/dns-query";
+    
+    var query_name = document.getElementById("dns-query").value;
+    var query_type = document.getElementById("dns-query-type").value;
+    
+    const result = await browser.storage.local.get("dns_url").then(
+        // https://stackoverflow.com/questions/29516390/how-can-i-access-the-value-of-a-promise
+        function (result) {
+            dns_base_url = result.dns_url || dns_base_url;
+    }).catch(function (error) {
+        console.log("Encountered error loading preferred DNS API URL.", error, "\nTrying query with default DNS API " + dns_base_url);
+    });
 
-                             The DNS APIs return these messages in the `Comment` field array
-                          */
-                          else if (result.Comment) {
-                              resultStr = "Server responded with a comment:\n";
-                              for (item of result.Comment) {
-                                  resultStr = resultStr + `${item}\n`;
-                              }
-                          }
-                          
-                          document.getElementById("result").innerText = resultStr;
-                          return result;
-                      }).catch(function (error) {
-                          console.log("error processing result:", error);
-                          return 1
-                      })
-            } else {
-                throw new Error("Unexpected Content-Type");
-            }
-        })
-        .catch((error) => {
-           console.log(`Error with fetch(): ${error.message}`)
-        });
-    }    
+    console.log("using DNS server", dns_base_url);
+    DnsAPIQuery(dns_base_url, query_name, query_type).then((response) => {
+        // Check if the response is ok
+        if (!response.ok) {
+          errMsg = `Error: ${response.status} - ${response.statusText}`;
+          console.log(errMsg);
+        }
+        // Check if the response is in JSON format
+        if (response.headers.get("Content-Type")
+            .includes("json")) {
+              response.json().then(
+                  function (result) {
+                    console.log(result);
+                    var resultStr = "";
+
+                    if (result.Answer) {
+                        for (answer of result.Answer) {
+                            var dataFmt = `${answer.name.toString()}, ${answer.data.toString()}`;
+                            resultStr = resultStr + `${dataFmt}\n`;
+                        }
+                    }
+
+                    /* This handles cases where response has no answer but a
+                       OPT (RFC6891) message. This includes RFC8914 errors
+                       https://developers.cloudflare.com/1.1.1.1/infrastructure/extended-dns-error-codes/
+                
+                       The DNS APIs return these messages in the `Comment` field array
+                    */
+                    else if (result.Comment) {
+                        resultStr = "Server responded with a comment:\n";
+                        for (item of result.Comment) {
+                            resultStr = resultStr + `${item}\n`;
+                        }
+                    }
+
+                    document.getElementById("result").innerText = resultStr;
+
+                  }).catch(function (error) {
+                      console.log("error processing result:", error);
+                  })
+        } else {
+            throw new Error("Unexpected Content-Type");
+        }
+    })
+    .catch((error) => {
+        console.log(`Error with fetch(): ${error.message}`);
+     });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   var clickyButton = document.querySelector('#send-query');
-  clickyButton.addEventListener('click', loadQuery);
+  clickyButton.addEventListener('click', processQuery);
 });
